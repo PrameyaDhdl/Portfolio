6167b1e06c0325706ebd3c94bd5d7b3c8f074cd7
Prameya <prameyadhaubhadel@gmail.com>
2023-09-22 12:35:46 +0545

    adding descriptions o project

--- portfolio/src/Components/UI/Portfolio.jsx
+++ portfolio/src/Components/UI/Portfolio.jsx
@@ -1,24 +1,63 @@
 import React, { useState } from "react";
+import uni from "../../assets/images/uni.png";
+import paws from "../../assets/images/paws.png"
+import hms from "../../assets/images/hms.png"
+import "tailwindcss/tailwind.css"; // Make sure to import Tailwind CSS
 
 const Portfolio = () => {
   const projects = [
     {
-      title: "TWINDER",
+      title: "PAWS",
+      summary: "An Innovative twist on traditional collars to making pethood a breeze and relaxation the norm",
       description:
-        "A live Geolocation app for finding tweets and Twitter users around you.",
-      photo: "/src/assets/projects/img1.png", // Add the correct image path
+        (<p>PAWS - The Smart Pet Collar redefines pet ownership with its all-in-one solution. 
+          It's not just a smart collar; it's a comprehensive answer to pet tracking and care.<br/><br/>
+        At its core, PAWS MVP uses advanced technology like the ESP32 microcontroller and NEO-6M 
+        GPS module to provide precise real-time GPS tracking. But it doesn't stop there. PAWS also
+         monitors your pet's activity levels and heart rate, ensuring you're always aware of their well-being.<br/><br/>
+        Safety is a top priority with an inbuilt LED light for visibility in low-light conditions.
+         But the innovation doesn't end there. PAWS has plans to integrate a PPG sensor for advanced health insights, 
+         develop a user-friendly app interface, introduce vet care illness alerts, and implement a robust database system.<br/><br/>
+        PAWS offers peace of mind, enhances pet care, and strengthens the bond between pets and their owners. It's the 
+        ultimate smart pet collar, combining GPS tracking, activity monitoring, safety features, and health insights in 
+        one streamlined package.</p>),
+      photo: paws, 
     },
     {
-      title: "LIVENTS",
+      title: "UK UniGalore",
+      summary: 'An Algorithm designed to locate and analyze the UK Universities according to the preference',
       description:
-        "A video streaming app with live Geolocation, for streaming events.",
-      photo: "/src/assets/projects/img2.png", // Add the correct image path
+        (
+          <p>In this technological study, an algorithm designed to help Times Higher Education writers quickly locate
+          and analyze survey data from UK universities as students are pressing educational institutions for more
+          (Woodall et al., 2012). The algorithm is a useful tool that enables reporters to easily access and evaluate
+          survey data, assisting their reporting efforts and assisting students in making educated judgments when
+          choosing universities.<br/><br/>
+          The study begins by emphasizing the significance of choosing the appropriate data structure and algorithm
+          to ensure efficiency and performance. A list of dictionaries was chosen as the data structure to allow for
+          structured storage and quick retrieval of university data via key-value pairs. For sorting and searching, the
+          method employs quicksort and binary search, respectively. Quicksort efficiently sorts the dataset by
+          separating it into smaller portions, whereas binary search searches sorted datasets quickly and effectively.
+          When interacting with survey data, these algorithms lead to enhanced performance and a better user
+          experience.<br/><br/>
+          Quick Sort is an O(n log n) average time complexity. In comparison, the average time complexity of Bubble
+          Sort and Insertion Sort is O(n2). The divide-and-conquer approach of Quick Sort, combined with its better
+          efficiency, makes it an excellent choice for sorting huge data sets and optimizing memory utilization.
+          Binary Search, on the other hand, stands out as a quick and efficient search algorithm with a time
+          complexity of O(log n). Binary Search quickly narrows down the required element by halving the search
+          space with each comparison, making it especially useful for sorted or easily sorted data but requires a
+          sorted dataset.<br/><br/>
+          The algorithm proved to be working fully and correctly by completing an assertion table and testing with
+          a respective assert statement.</p>
+        ),
+      photo: uni, 
     },
     {
-      title: "MOOVE",
+      title: "Hospital Management System",
+      summary: 'An Algorithm designed to locate and analyze the UK Universities according to the preference',
       description:
         "Mobile app for booking instant pickup & drop-off across major cities.",
-      photo: "/src/assets/projects/img3.png", // Add the correct image path
+      photo: hms, 
     },
     // Add more projects as needed...
   ];
@@ -34,7 +73,7 @@ const Portfolio = () => {
   };
 
   return (
-    <div className=" px-7 mt-16 text-center md:px-56" id="portfolio">
+    <div className="px-7 mt-16 text-center md:px-56" id="portfolio">
       <h2 className="text-headingColor font-[800] text-[2.4rem] mb-5 mt-5">
         My <span className="text-primaryColor">Portfolio</span>
       </h2>
@@ -45,7 +84,9 @@ const Portfolio = () => {
       </p>
 
       {/* Featured projects */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 my-6 items-center justify-center">
+      <div  data-aos='fade-up' 
+            data-aos-duration='1500'
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 my-6 items-center justify-center">
         {projects.map((project, index) => {
           return (
             <div
@@ -59,12 +100,12 @@ const Portfolio = () => {
                 rel="noreferrer"
                 className="mb-4"
               >
-                <img src={project.photo} alt={project.title} />
+                <img src={project.photo} alt={project.title} className="max-w-[100%] h-auto" />
               </a>
               <h3 className="text-primaryColor font-[600] text-[23px]">
                 {project.title}
               </h3>
-              <p className=" text-white mt-1">{project.description}</p>
+              <p className="text-white mt-1">{project.summary}</p>
               <div className="mt-5">
                 <button
                   className="font-[500] bg-primaryColor py-2 px-6 rounded text-smallTextColor hover:bg-white"
@@ -87,18 +128,21 @@ const Portfolio = () => {
       {/* Description pages */}
       {selectedProject !== null && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-90">
-          <div className="bg-white p-4 rounded shadow-md text-center">
+          <div className="bg-white p-4 rounded shadow-md text-center max-w-[90%] md:max-h-[80%] max-h-[75%] overflow-y-auto">
             <h3 className="text-primaryColor font-[600] text-[23px]">
               {projects[selectedProject].title}
             </h3>
-            <img
-              src={projects[selectedProject].photo}
-              alt={projects[selectedProject].title}
-              className="mx-auto my-4 max-w-full h-auto"
-            />
-            <p className="text-smallTextColor mt-1">
-              {projects[selectedProject].description}
-            </p>
+            <div className=" flex md:flex-row flex-col">
+              <img
+                src={projects[selectedProject].photo}
+                alt={projects[selectedProject].title}
+                className="mx-auto my-4 max-w-[90%] h-auto"
+              />
+              <p className="text-smallTextColor mt-1 p-4 font-[500] text-[16px] text-justify">
+                {projects[selectedProject].description}
+              </p>
+            </div>
+            
             <button
               className="outline py-1.5 px-9 font-[500] rounded border-none mt-4 text-primaryColor"
               onClick={closeDescription}
